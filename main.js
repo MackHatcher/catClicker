2a1c983b35ec682d243adda8ee118cd41ce628cf
Mack-Hatcher <mack.hatcher1@outlook.com>
2018-05-17 10:47:12 -0500

    added step 2

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     let counter = document.getElementById("catCounter");
+    let counter2 = document.getElementById('catCounter2');
+    let catName = document.getElementById('catName');
+    let catName2 = document.getElementById('catName2');
+
+    catName.textContent = "Mittens:";
+    catName2.textContent = "Boots:";
     
     let kittyKat = document.getElementById('kitty');
     kittyKat.addEventListener('click', function () {
         counter.innerHTML++;
 })
+    let kittyKat2 = document.getElementById('kitty2');
+    kittyKat2.addEventListener('click', function() {
+        counter2.innerHTML++;
+    })
 });
